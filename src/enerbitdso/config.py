bb41cdfb9ad71101908cf8220606866f545ef552
pablete <jtamayoh@gmail.com>
2024-11-01 18:11:01 -0500

    move tests and use uv pyproject

--- src/enerbitdso/config.py
+++ src/enerbitdso/config.py
@@ -2,12 +2,8 @@ import pydantic
 
 
 class Config(pydantic.BaseSettings):
-    DAY_FILENAME_TEMPLATE: str = (
-        "STAR_{operation_day:%Y%m%d}_{serial}_Day_{now:%Y%m%d%H%M%S}.xml"
-    )
-    INTERVAL_FILENAME_TEMPLATE: str = (
-        "STAR_{operation_day:%Y%m%d}_{serial}_Interval_{now:%Y%m%d%H%M%S}.xml"
-    )
+    DAY_FILENAME_TEMPLATE: str = "STAR_{operation_day:%Y%m%d}_{serial}_Day_{now:%Y%m%d%H%M%S}.xml"
+    INTERVAL_FILENAME_TEMPLATE: str = "STAR_{operation_day:%Y%m%d}_{serial}_Interval_{now:%Y%m%d%H%M%S}.xml"
 
 
 settings = Config()
